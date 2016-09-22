f703719acdde71fb85d048ac6e44437d44651292
Farrel Buchinsky <fjbuch@gmail.com>
2016-09-22 17:32:03 -0400

    First Committ
    I am trying this for the first time on my work computer

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 WorkCompFirstGitKraken
+
+# Trying the first  
+I do not know what I am doing but I must go now
---
+++ Testing gmailR.R
@@ -0,0 +1,94 @@
+library(googlesheets)
+library(gmailr)
+library(data.table)
+library(stringr)
+library(purrr)
+
+testemail <- data.table(first=c("Jacob", "Adam", "Farrel", "Jennifer"), last=c("Buchinsky", "Buchinsky", "Buchinsky", "Buchinsky"), email=c("jacob.buchinsky@gmail.com", "adam.s.buchinsky@gmail.com", "farrel.buchinsky@ahn.org", "jbuchinsky@gmail.com"), owe=c(0.53, 0.54, 0.25, 1.65))
+
+testemail[, body:=str_c("Dear ", first, "
+												I was going through Quicken and it looks as if I owe you ", owe, " USD. Is that possible. If you think this question is odd then you are correct. I am just trying something out.
+												Farrel")]
+
+cat("Dear ", "first", "\n I was going through Quicken and it looks as if I owe you ", "5", " USD. Is that possible. If you think this question is odd then you are correct. I am just trying something out.
+			Farrel")
+
+body <- "Dear %s
+
+I was going through Quicken and it looks as if I owe you %s USD. Is that possible? If you think this question is odd then you are correct. I am just trying something out.
+
+Bye
+Farrel"
+testemail[, body:=sprintf(body, first, owe)]
+
+purrr::pmap(mime(
+	To = "jbuchinsky@gmail.com",
+	From = "fjbuch@gmail.com",
+	Subject = "this is just a gmailr test",
+	body = "Can you hear me now?")
+))
+dat  <- mime(
+	To = "jbuchinsky@gmail.com",
+	From = "fjbuch@gmail.com",
+	Subject = "this is just a gmailr test",
+	body = "Can you hear me now?")
+send_message(test_email)
+
+class(dat)
+
+check.details.reminder[, emailsend := mime( To = email, From  = "fjbuch@gmail.com", Subject = "You have not yet checked your details", body = str_c("Dear ", first, ", Six collaborators have entered the date (yyyy-mm-dd) on which they checked their details on the collaborators spreadhseet. https://docs.google.com/spreadsheets/d/1h01etGN9mrN6Tu5rlE_A0w027sC_2KR2LE7BZpRuoaM/edit?usp=sharing    You were not one of them. The abstract will be submitted on Monday 2016-09-12. Please make sure I have correct information on you. If you encounter an error please edit the spreadsheet directly. Feel free to email me or call me on my mobile (412) 567-7870 if you have any questions. Thanks, Farrel"))]
+
+check.details.reminder[, body := as.character(str_c("Dear ", first, ", Six collaborators have entered the date (yyyy-mm-dd) on which they checked their details on the collaborators spreadhseet. https://docs.google.com/spreadsheets/d/1h01etGN9mrN6Tu5rlE_A0w027sC_2KR2LE7BZpRuoaM/edit?usp=sharing    You were not one of them. The abstract will be submitted on Monday 2016-09-12. Please make sure I have correct information on you. If you encounter an error please edit the spreadsheet directly. Feel free to email me or call me on my mobile (412) 567-7870 if you have any questions. Thanks, Farrel"))]
+
+
+suppressPackageStartupMessages(library(gmailr))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(purrr))
+library(readr)
+getwd()
+addresses <- read_csv("addresses.csv")
+marks <- read_csv("marks.csv")
+my_dat <- left_join(marks, addresses)
+
+this_hw <- "The Fellowship Of The Ring"
+email_sender <- 'Peter Jackson <peter@tolkien.example.org>' # your Gmail address
+optional_bcc <- 'Anonymous <anon@palantir.example.org>'     # for me, TA address
+body <- "Hi, %s.
+Your mark for %s is %s.
+Thanks for participating in this film!
+"
+
+edat <- my_dat %>%
+	mutate(
+		To = sprintf('%s <%s>', name, email),
+		Bcc = optional_bcc,
+		From = email_sender,
+		Subject = sprintf('Mark for %s', this_hw),
+		body = sprintf(body, name, this_hw, mark)) %>%
+	select(To, Bcc, From, Subject, body)
+edat
+data.table(edat)
+write_csv(edat, "composed-emails.csv")
+
+emails <- edat %>%
+	pmap(mime)
+
+# email2 <- pmap(edat, mime)
+#
+# identical(emails, email2)
+# str(email2)
+## optional: use if you've created your own client id
+#use_secret_file("gmailr-tutorial.json")
+
+safe_send_message <- safely(send_message)
+sent_mail <- emails %>%
+	map(safe_send_message)
+
+saveRDS(sent_mail,
+				paste(gsub("\\s+", "_", this_hw), "sent-emails.rds", sep = "_"))
+
+errors <- sent_mail %>%
+	transpose() %>%
+	.$error %>%
+	map_lgl(Negate(is.null))
+sent_mail[errors]
