ed23c5e1248be42c2ed769b3cfc318a8cc18ec08
Farrel Buchinsky <fjbuch@gmail.com>
2016-09-23 17:20:40 -0400

    the all knowing
    My first commit with github for windows but I never added. Although
    there was a check in the checkbox

--- Testing gmailR.R
+++ Testing gmailR.R
@@ -5,7 +5,7 @@ library(stringr)
 library(purrr)
 
 
-#trying out Friday, 23 Sep 2016 17:08
+#trying out Friday, 23 Sep 2016 17:19 and all that
 
 testemail <- data.table(first=c("Jacob", "Adam", "Farrel", "Jennifer"), last=c("Buchinsky", "Buchinsky", "Buchinsky", "Buchinsky"), To=c("jacob.buchinsky@gmail.com", "adam.s.buchinsky@gmail.com", "farrel.buchinsky@ahn.org", "jbuchinsky@gmail.com"), owe=c(0.53, 0.54, 0.25, 1.65), From = "Farrel Buchinsky<fjbuch@gmail.com")
 
